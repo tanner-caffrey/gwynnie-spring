2fa92c2563b81288292d55697a603a8639071196
tanner.caffrey <tanner.caffrey@gmail.com>
2025-05-01 14:45:15 -0400

    Remove unused imports

--- backpaws/src/main/java/gay/gwynnie/backpaws/photo/PhotoController.java
+++ backpaws/src/main/java/gay/gwynnie/backpaws/photo/PhotoController.java
@@ -6,14 +6,12 @@ import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
