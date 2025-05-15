d439b5be0e6b57efa0178de65f186746c815c9ac
tanner.caffrey <tanner.caffrey@gmail.com>
2025-05-15 13:16:58 -0400

    Add upload and gallery, initially.

--- backpaws/src/main/java/gay/gwynnie/backpaws/pages/PageController.java
+++ backpaws/src/main/java/gay/gwynnie/backpaws/pages/PageController.java
@@ -1,26 +1,26 @@
 package gay.gwynnie.backpaws.pages;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import gay.gwynnie.backpaws.photo.Photo;
 import gay.gwynnie.backpaws.photo.PhotoService;
 
 @Controller
 public class PageController {
 
-    @Autowired
-    private PhotoService photoService;
+  @Autowired
+  private PhotoService photoService;
 
+  @GetMapping("/upload")
+  public String uploadPage() {
+    return "forward:/upload.html";
+  }
 
-    @GetMapping("/gallery")
+  @GetMapping("/gallery")
     public String gallery(Model model) {
-        List<Photo> photos = photoService.getAllPhotos();
-        model.addAttribute("photos", photos);
+        model.addAttribute("photos", photoService.getAllPhotos());
         return "gallery";   // resolves to templates/gallery.html
     }
 }
